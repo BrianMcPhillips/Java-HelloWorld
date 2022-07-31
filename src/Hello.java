cccfc85db9ea08d22b70f33126d274a8f0a0ac93
Brian McPhillips <B.McPhillips89@gmail.com>
2022-07-31 16:19:36 -0400

    Print two variables with operator

--- src/Hello.java
+++ src/Hello.java
@@ -6,10 +6,16 @@ public class Hello {
         int myFirstNumber = 5;
         int mySecondNumber = 5 + 10;
         int myThirdNumber = (5 * 2) + 10;
+        int myFourthNumber = (100 * 100) * 100;
+        int myFifthNumber = 6;
+        int mySixthNumber = 12;
+
 
         System.out.println(myFirstNumber);
         System.out.println(mySecondNumber);
         System.out.println(myThirdNumber);
+        System.out.println(myFourthNumber);
+        System.out.println(myFifthNumber * mySixthNumber);
 
     }
 
