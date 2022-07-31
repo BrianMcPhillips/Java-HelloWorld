6ec0a626e6eb043eb701e0efdce13f8f2bca0ad3
Brian McPhillips <B.McPhillips89@gmail.com>
2022-07-31 12:29:07 -0400

    More complexity to variable

--- src/Hello.java
+++ src/Hello.java
@@ -5,9 +5,11 @@ public class Hello {
 
         int myFirstNumber = 5;
         int mySecondNumber = 5 + 10;
+        int myThirdNumber = (5 * 2) + 10;
 
         System.out.println(myFirstNumber);
         System.out.println(mySecondNumber);
+        System.out.println(myThirdNumber);
 
     }
 
